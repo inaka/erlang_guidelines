34c47bda9601f179a84512131ca8d7dc0dbe5d81
Iñaki Garay <igarai@gmail.com>
2014-08-05 16:44:48 -0300

    Added example file to mark repo as erlang.

---
+++ examples.erl
@@ -0,0 +1,14 @@
+-module(examples).
+
+%%% BAD
+-exports([ function1
+         , i_like
+         , preceding_commas, i_dont, like_them]).
+
+%%% GOOD
+-exports([ function1
+         , i_like
+         , preceding_commas
+         , i_dont
+         , like_them
+         , but_i_respect_your_style]).
