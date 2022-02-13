ce441f46220085a60abcd5e6f0a7e56c36cb0c5d
Kian-Meng Ang <kianmeng@cpan.org>
2022-02-13 16:57:32 +0800

    Fix typos

--- src/dry.erl
+++ src/dry.erl
@@ -5,15 +5,15 @@
 -export([bad/0, good/0]).
 
 bad() ->
-  case somthing:from(other, place) of
+  case something:from(other, place) of
     {show, _} ->
-      display:nicely(somthing:from(other, place));
+      display:nicely(something:from(other, place));
     nothing ->
       display:nothing()
   end.
 
 good() ->
-  case somthing:from(other, place) of
+  case something:from(other, place) of
     {show, _} = ThingToShow ->
       display:nicely(ThingToShow);
     dont_show_me ->
